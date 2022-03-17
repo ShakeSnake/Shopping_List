c789b02811f8c5f3f78ffae3b360414b5420646e
Alexey Keliy <shakesnake526@gmail.com>
2022-03-17 18:38:29 +0300

    Added LiveData

--- app/src/main/java/com/shakesnake/myapplication/domain/GetShopListUseCase.kt
+++ app/src/main/java/com/shakesnake/myapplication/domain/GetShopListUseCase.kt
@@ -1,7 +1,10 @@
 package com.shakesnake.myapplication.domain
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
-    fun getShopList(): List<ShopItem> {
+    fun getShopList(): LiveData<List<ShopItem>> {
        return shopListRepository.getShopList()
     }
 }
