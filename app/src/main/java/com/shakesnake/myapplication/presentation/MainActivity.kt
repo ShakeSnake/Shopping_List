3b1e136ac399df02388fd332787f023f526a3a2c
Alexey Keliy <shakesnake526@gmail.com>
2022-04-02 16:17:11 +0300

    Added intents with extras from MainActivity to ShopItemActivity

--- app/src/main/java/com/shakesnake/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/shakesnake/myapplication/presentation/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.shakesnake.myapplication.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.shakesnake.myapplication.R
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +20,14 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
-            Log.d("MainActivityTest", it.toString())
+        }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
+
     private fun setupRecyclerView() {
         val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
         with(rvShopList) {
@@ -67,7 +72,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         shopListAdapter.onShopItemClickListener = {
-            Log.d("ShopItemTag", it.toString())
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
